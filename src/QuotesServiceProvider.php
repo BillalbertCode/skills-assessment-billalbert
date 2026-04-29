0096bb41b781dfa9c8ed3b5ec0435ab3f09a168f
billalbertcode <billalbertcode@gmail.com>
2026-04-28 21:24:25 -0400

    Feature add QuoresProvider Service

---
+++ src/QuotesServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dustov\Quotes;
+
+use Illuminate\Support\ServiceProvider;
+
+class QuotesServiceProvider extends ServiceProvider{
+
+
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+        
+    }
+}
